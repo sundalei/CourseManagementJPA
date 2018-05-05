efed126710b85e44472d87490ec8fb658d9cf244
sundalei <sundalei2011@163.com>
2018-05-05 19:19:10 +0800

    tttt

--- src/main/java/packt/book/jee/eclipse/ch4/jpa/service_bean/CourseServiceBean.java
+++ src/main/java/packt/book/jee/eclipse/ch4/jpa/service_bean/CourseServiceBean.java
@@ -1,10 +1,14 @@
 package packt.book.jee.eclipse.ch4.jpa.service_bean;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 import packt.book.jee.eclipse.ch4.jpa.bean.Course;
+import packt.book.jee.eclipse.ch4.jpa.service.CourseService;
 
 @ManagedBean(name="courseServiceBean")
 @RequestScoped
@@ -13,7 +17,17 @@ public class CourseServiceBean {
 	@ManagedProperty(value="#{course}")
 	private Course course;
 	
+	@ManagedProperty(value="#{emFactoryBean}")
+	private EntityManagerFactoryBean entityManagerFactoryBean;
+	
 	private String errMsg = null;
+	
+	private CourseService courseService;
+	
+	@PostConstruct
+	public void init() {
+		courseService = new CourseService(entityManagerFactoryBean);
+	}
 
 	public Course getCourse() {
 		return course;
@@ -33,6 +47,15 @@ public class CourseServiceBean {
 	
 	public String addCourse() {
 		//System.out.println(course.getName());
+		courseService.addCourse(course);
 		return "listCourse";
+	}
+	
+	public List<Course> getCourses() {
+		return courseService.getCourses();
+	}
+
+	public void setEntityManagerFactoryBean(EntityManagerFactoryBean entityManagerFactoryBean) {
+		this.entityManagerFactoryBean = entityManagerFactoryBean;
 	}
 }
